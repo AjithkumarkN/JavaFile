1e39918b4db402f079a45041de0db2686bd5efe6
AjithkumarkN <aka39684@gmail.com>
2022-08-05 22:36:38 -0700

    simple pattern program

---
+++ SimplePattern.java
@@ -0,0 +1,11 @@
+public class SimplePattern {
+    public static void main(String [] args){
+        //we need two for loop each loop denote row and columns
+        for(int i=1;i<=5;i++) {//denote row value
+            for (int j = 1; j <= 5; j++) {//it's denote column value
+                System.out.print("*");//print the *
+            }
+            System.out.println();//goto next line
+        }
+    }
+}
