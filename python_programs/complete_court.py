60753718071ceb8442859d7237fb76241b15a11c
AjithkumarkN <110447288+AjithkumarkN@users.noreply.github.com>
2022-10-02 09:11:37 -0700

    dual court address extraction completed

--- python_programs/complete_court.py
+++ python_programs/complete_court.py
@@ -1,4 +1,4 @@
-from msilib.schema import Class
+from msilib.schema import Class, ODBCTranslator
 from operator import index, indexOf
 from re import I
 from traceback import print_tb
@@ -8,45 +8,53 @@ from selenium.webdriver.common.by import By
 import time
 from selenium.webdriver.chrome.service import Service
 import pandas as pd
+from selenium.webdriver.chrome.options import Options
+import csv
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
  
 
+df = pd.read_csv("D:\AK\Pdf_to_text\dual_court\Copy of lispendens of september - 01_09_2022.csv")
+data=df.iloc[:,4:5].values
+chrome_options = Options()  
+chrome_options.add_argument("--headless") 
 s = Service('D:\AK\ChromeDriver\chromedriver.exe')
-driver = webdriver.Chrome(service=s)
+driver = webdriver.Chrome(service=s,chrome_options=chrome_options)
 url='https://core.duvalclerk.com/CoreCms.aspx'
 driver.get(url)
-driver.find_element('xpath','//*[@id="c_UsernameTextBox"]').send_keys('sean@ozellrealestate.com')
-driver.find_element('xpath','//*[@id="c_PasswordTextBox"]').send_keys('RealEstate45!')
-time.sleep(1)
-driver.find_element('xpath','//*[@id="LoginDialog"]/tbody/tr[4]/td[2]/input').click()
-google_sheetId="1NypSapo7XLamkby-wuDfmhP08Lc0G_TZEgP0i7G2ysA"
-sheet_name="Datavalue"
+driver.implicitly_wait(1)
+driver.find_element(By.CSS_SELECTOR,"input[name='ctl00$c_UsernameTextBox']").send_keys('Username')
+driver.find_element(By.CSS_SELECTOR,"input[name='ctl00$c_PasswordTextBox']").send_keys('password')
 
-gsheet_url = "https://docs.google.com/spreadsheets/d/{}/gviz/tq?tqx=out:csv&sheet={}".format(google_sheetId, sheet_name)
-print(gsheet_url)
-df = pd.read_csv(gsheet_url)
-data=df.iloc[:,4:5].values
+driver.find_element(By.CSS_SELECTOR,"input[value='Login to CORE']").click()
+to_csv1=[]
 for i in data:
-    time.sleep(2)
+        
     driver.find_element(By.CSS_SELECTOR,'body > form:nth-child(1) > div:nth-child(35) > div:nth-child(3) > div:nth-child(4) > div:nth-child(1) > div:nth-child(2) > div:nth-child(3) > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(1) > input:nth-child(1)').send_keys(i)
-    time.sleep(2)
     driver.find_element(By.CSS_SELECTOR,'body > form:nth-child(1) > div:nth-child(35) > div:nth-child(3) > div:nth-child(4) > div:nth-child(1) > div:nth-child(2) > div:nth-child(3) > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(4) > td:nth-child(1) > input:nth-child(1)').click()
-    time.sleep(7)
-    table = driver.find_element(By.XPATH,("//span[@id='c_CaseNumberLabel']")).text
-    print(table)
-    table1 = driver.find_element(By.XPATH,("//div[@class='caseDisplayTable caseSummary']/table/tbody/tr[2]/td[1]")).text
-    table2 = driver.find_element(By.XPATH,("//div[@class='caseDisplayTable caseSummary']/table/tbody/tr[2]/td[2]")).text
-    print(table2)
-    if (table2=='OPEN'):
-        table3 = driver.find_element(By.XPATH,("//div[@class='caseDisplayTable']/table/tbody[2]/tr/td[1]")).text
-        table4 = driver.find_element(By.XPATH,("//div[@class='caseDisplayTable']/table/tbody[2]/tr/td[2]")).text
-        table5 = driver.find_element(By.XPATH,("//div[@class='caseDisplayTable']/table/tbody[2]/tr/td[3]")).text
-        table_value=(table,table1,table2,table3,table4.strip().replace("\n"," "),table5.replace("\n", " "))
-        print(table_value)
-        file=open("D:\AK\Pdf_to_text\dual_court\Data_files4.txt",'a')
-        print(file.write("{}\n".format(table_value)))
+    try:
+        table = driver.find_element(By.XPATH,("//span[@id='c_CaseNumberLabel']")).text
+        print(table)
+        table1 = driver.find_element(By.XPATH,("//div[@class='caseDisplayTable caseSummary']/table/tbody/tr[2]/td[2]")).text
+        print(table1)
+        if (table1=='OPEN'):
+            table2 = driver.find_element(By.XPATH,("//div[@class='caseDisplayTable']/table/tbody[2]/tr/td[1]")).text
+            table3 = driver.find_element(By.XPATH,("//div[@class='caseDisplayTable']/table/tbody[2]/tr/td[2]")).text
+            table4 = driver.find_element(By.XPATH,("//div[@class='caseDisplayTable']/table/tbody[2]/tr/td[3]")).text
+            to_csv = [
+                    {'Caser_ID': table, 'Case_Status': table1, 'Petitioner_Name': table2, 'Party_Type':table3, 'Address':table4},
+                    
+                    ]
+            to_csv1+=to_csv
+            driver.refresh()
+        else:
+            driver.refresh()
+    except NoSuchElementException:
         driver.refresh()
-    else:
-        driver.refresh()
-read_file = pd.read_csv ('D:\AK\Pdf_to_text\dual_court\Data_files4.txt')
-read_file.to_csv ('D:\AK\Pdf_to_text\dual_court\Data_files4.csv', index=None)       
-
+    
+keys = to_csv[0].keys()
+with open('D:\AK\Pdf_to_text\dual_court\people.csv', 'w', newline='') as output_file:
+    dict_writer = csv.DictWriter(output_file, keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(to_csv1)
