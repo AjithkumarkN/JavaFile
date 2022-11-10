542e6ed257e4cbf8872cb9557ae9269bdc90e8b8
AjithkumarkN <110447288+AjithkumarkN@users.noreply.github.com>
2022-11-10 19:03:44 +0530

    Divided a class as sub module in dual program

--- python_programs/DualExtractCompleted.py
+++ python_programs/DualExtractCompleted.py
@@ -11,11 +11,9 @@ import csv
 import zipfile
 import xmltodict
 import time
-class tenant_And_Lispendens():
+class FtpFile_Download():
     options = Options()  
     options.add_argument("--headless") 
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
     downloads_path = str(Path.home() / "Downloads")
     chrome_prefs = {"download.default_directory":downloads_path} 
     options.experimental_options["prefs"] = chrome_prefs
@@ -24,8 +22,8 @@ class tenant_And_Lispendens():
     clerk_url='https://ftp.duvalclerk.com/'
     driver.get(clerk_url)
     driver.implicitly_wait(2)
-    driver.find_element(By.CSS_SELECTOR,'#user-box-text').send_keys('ftp_dualusername')
-    driver.find_element(By.CSS_SELECTOR,'#pword-box-text').send_keys('ftp_dualpassword')
+    driver.find_element(By.CSS_SELECTOR,'#user-box-text').send_keys('ftp_username')
+    driver.find_element(By.CSS_SELECTOR,'#pword-box-text').send_keys('ftp_password')
     driver.find_element(By.CSS_SELECTOR,'#btn-LoginButton').click()
     click_action=ActionChains(driver)
     casefile_folder=driver.find_element(By.CSS_SELECTOR,'#ListFiles-row-0')
@@ -36,45 +34,50 @@ class tenant_And_Lispendens():
     a=[]
     a.append(file_location.text.split('.'))
     filename=a[0][0]
-    time.sleep(2)
-    downloads_path = str(Path.home() / "Downloads")
-    zip_path=os.path.join(downloads_path,filename+'.zip')
-    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-        zip_ref.extractall(downloads_path)
-    xml_path=os.path.join(downloads_path,filename+'.xml')
-    xsd_path=os.path.join(downloads_path,filename+'.xsd')
-    with open(xml_path, 'r') as xmlfile:
-        xmlfiledata = xmlfile.read()
-    xmlfiledata_dict = xmltodict.parse(xmlfiledata)
-    petitionar_casedatas = [dict(x) for x in xmlfiledata_dict["Cases"]["Case"]]
-    HEADERS = ['CaseID', 'Department', 'DivisionName', 'CaseTypeDescription', 'CaseNumber', 'FileDate']
-    filerows = []
-    for petitionar_casedata in petitionar_casedatas:
-        CaseID = petitionar_casedata["CaseID"]
-        Department= petitionar_casedata["Department"]
-        DivisionName= petitionar_casedata["DivisionName"]
-        CaseTypeDescription = petitionar_casedata["CaseTypeDescription"]
-        CaseNumber= petitionar_casedata["CaseNumber"]
-        FileDate= petitionar_casedata["FileDate"]
-        filerows.append([CaseID,Department,DivisionName,CaseTypeDescription,CaseNumber,FileDate])
-    csvfile_path=os.path.join(downloads_path,filename+'.csv')
-    with open(csvfile_path, 'w',newline="") as csvfile_write:
-        caseDetail_write = csv.writer(csvfile_write)
-        caseDetail_write.writerow(HEADERS)
-        caseDetail_write.writerows(filerows)
-    today_dateTime = date.today()
-    today_Date = today_dateTime.strftime("%Y-%m-%d")    
-    convert_tenantfile=os.path.join(downloads_path,'tenent-'+today_Date+'.csv')
-    convert_lispendensefile=os.path.join(downloads_path,'lispendence-'+today_Date+'.csv')
+    def extract_zipfile_path(self):
+        zip_path=os.path.join(self.downloads_path,self.filename+'.zip')
+        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+            zip_ref.extractall(self.downloads_path)
+        xsd_path=os.path.join(self.downloads_path,self.filename+'.xsd')
+        xml_path=os.path.join(self.downloads_path,self.filename+'.xml')
+        object.xmlfile_to_csvfile(xml_path)
+        os.remove(xsd_path) 
+        os.remove(zip_path) 
+    def xmlfile_to_csvfile(self,xml_path):        
+        with open(xml_path, 'r') as xmlfile:
+            xmlfiledata = xmlfile.read()
+        xmlfiledata_dict = xmltodict.parse(xmlfiledata)
+        petitionar_casedatas = [dict(x) for x in xmlfiledata_dict["Cases"]["Case"]]
+        HEADERS = ['CaseID', 'Department', 'DivisionName', 'CaseTypeDescription', 'CaseNumber', 'FileDate']
+        filerows = []
+        for petitionar_casedata in petitionar_casedatas:
+            CaseID = petitionar_casedata["CaseID"]
+            Department= petitionar_casedata["Department"]
+            DivisionName= petitionar_casedata["DivisionName"]
+            CaseTypeDescription = petitionar_casedata["CaseTypeDescription"]
+            CaseNumber= petitionar_casedata["CaseNumber"]
+            FileDate= petitionar_casedata["FileDate"]
+            filerows.append([CaseID,Department,DivisionName,CaseTypeDescription,CaseNumber,FileDate])
+        csvfile_path=os.path.join(self.downloads_path,self.filename+'.csv')
+        with open(csvfile_path, 'w',newline="") as csvfile_write:
+            caseDetail_write = csv.writer(csvfile_write)
+            caseDetail_write.writerow(HEADERS)
+            caseDetail_write.writerows(filerows)
+        os.remove(xml_path)
+class Tenant_And_Lispendens(FtpFile_Download):
+    driver=FtpFile_Download.driver
     court_url='https://core.duvalclerk.com/CoreCms.aspx'
     driver.get(court_url)
     driver.implicitly_wait(10)
-    driver.find_element(By.CSS_SELECTOR,"input[name='ctl00$c_UsernameTextBox']").send_keys('dualcourt_username')
-    driver.find_element(By.CSS_SELECTOR,"input[name='ctl00$c_PasswordTextBox']").send_keys('dualcourt_password')
-    driver.find_element(By.CSS_SELECTOR,"input[value='Login to CORE']").click()
+    driver.find_element(By.CSS_SELECTOR,"input[name='ctl00$c_UsernameTextBox']").send_keys('dual_username')
+    driver.find_element(By.CSS_SELECTOR,"input[name='ctl00$c_PasswordTextBox']").send_keys('dual_password')
+    driver.find_element(By.CSS_SELECTOR,"input[value='Login to CORE']").click()  
+    today_dateTime = date.today()
+    today_Date = today_dateTime.strftime("%Y-%m-%d")
     teant_caseDtails=[]
-    lispendense_caseDetails=[]
-    def get_casenumber(self,csvfile_path):
+    lispendense_caseDetails=[]  
+    def get_casenumber(self):
+        csvfile_path=os.path.join(self.downloads_path,self.filename+'.csv')
         case_Details=[*csv.DictReader(open(csvfile_path))]
         for case_Detail in case_Details:
             case_Type=case_Detail['CaseTypeDescription'].upper()
@@ -82,13 +85,15 @@ class tenant_And_Lispendens():
                 case_Number=case_Detail['CaseNumber']
                 file_date_time=case_Detail['FileDate'].split('T')
                 file_Date=file_date_time[0]
-                obj.tenant_Lispendens_SearchCaseStatus(case_Number,file_Date,case_Type)
+                obj.tenant_lispendens_searchCaseStatus(case_Number,file_Date,case_Type)
             elif 'FORECLOSURE' in case_Type:
                 case_Number=case_Detail['CaseNumber']
                 file_date_time=case_Detail['FileDate'].split('T')
                 file_Date=file_date_time[0]
-                obj.tenant_Lispendens_SearchCaseStatus(case_Number,file_Date,case_Type) 
-    def tenant_Lispendens_SearchCaseStatus(self,case_Number,file_Date,case_Type):
+                obj.tenant_lispendens_searchCaseStatus(case_Number,file_Date,case_Type)
+        obj.write_csvfile() 
+        os.remove(csvfile_path)
+    def tenant_lispendens_searchCaseStatus(self,case_Number,file_Date,case_Type):
         try:
             self.driver.find_element(By.CSS_SELECTOR,'body > form:nth-child(1) > div:nth-child(35) > div:nth-child(3) > div:nth-child(4) > div:nth-child(1) > div:nth-child(2) > div:nth-child(3) \
             > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(1) > input:nth-child(1)').send_keys(case_Number)
@@ -116,7 +121,7 @@ class tenant_And_Lispendens():
                     self.teant_caseDtails+=tenant_casestatus
                     self.driver.refresh()
                 else:
-                    self.driver.refresh()            
+                    self.driver.refresh()        
             elif 'FORECLOSURE' in case_Type:        
                 if (case_Status=='OPEN'):
                     lispendense_casestatus = [
@@ -125,21 +130,22 @@ class tenant_And_Lispendens():
                     self.lispendense_caseDetails+=lispendense_casestatus
                     self.driver.refresh()
                 else:
-                    self.driver.refresh()    
+                    self.driver.refresh()  
         except NoSuchElementException:
             self.driver.refresh()
+    def write_csvfile(self):
+        convert_tenantfile=os.path.join(self.downloads_path,'tenent-'+self.today_Date+'.csv')
+        convert_lispendensefile=os.path.join(self.downloads_path,'lispendence-'+self.today_Date+'.csv')
         csv_Headings=['Case_ID','Case_Status','Petitioner_Name','Party_Type','Address']
-        with open(self.convert_tenantfile, 'w', newline='') as tenant_outputfile:
+        with open(convert_tenantfile, 'w', newline='') as tenant_outputfile:
             tenant_dict_writer = csv.DictWriter(tenant_outputfile, fieldnames=csv_Headings)
             tenant_dict_writer.writeheader()
             tenant_dict_writer.writerows(self.teant_caseDtails)
-        with open(self.convert_lispendensefile, 'w', newline='') as lispendense_outputfile:
+        with open(convert_lispendensefile, 'w', newline='') as lispendense_outputfile:
             lispendense_dict_writer = csv.DictWriter(lispendense_outputfile, fieldnames=csv_Headings)
             lispendense_dict_writer.writeheader()
             lispendense_dict_writer.writerows(self.lispendense_caseDetails)
-obj=tenant_And_Lispendens()
-obj.get_casenumber(obj.csvfile_path)
-os.remove(obj.zip_path)
-os.remove(obj.xml_path)
-os.remove(obj.xsd_path)            
-os.remove(obj.csvfile_path)
+object=FtpFile_Download()
+object.extract_zipfile_path()
+obj=Tenant_And_Lispendens()
+obj.get_casenumber()
